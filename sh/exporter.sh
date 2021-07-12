f8752bdb063edef6e5d3256654fb77fd1d218643
Maksymilian Potok <makspotok@gmail.com>
2021-07-12 14:30:09 +0200

    added curl/file checking

--- sh/exporter.sh
+++ sh/exporter.sh
@@ -3,24 +3,35 @@
 set -e
 set -u
 
-NODE_TAR="*node*.tar.gz"
+NODE_TAR="node*.tar.gz"
 NODE_TAR_TMP="temp.tar.gz"
 PROM_DIR="/etc/prometheus"
 NODE_DIR=$(find "$PROM_DIR" -type d -name "node*")
 FIREWALL_RULES=$(iptables -S | grep "9100" | sed 's/.* //g')
 
+BIN="amd64"
+
+LATEST=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "linux-${BIN}.tar.gz" | cut -d '"' -f 4 | tail -1)
+
 if [[ "$FIREWALL_RULES" ]]; then
     echo "OK"
 else
-    echo "You need to allow your firewall on port 9090"
+    echo "Your firewall is off or you need to allow your firewall on port 9090"
+    echo "Continue? [Y/n]"
+    read IN
+    if [ ! IN == "Y" ]; then
+        exit 1
+    fi
 fi
 
 cd "$PROM_DIR"
 
-if [ -f "$NODE_TAR" ]; then
-    mv "$NODE_TAR" "$NODE_TAR_TMP"
+if [ ! -f "$PROM_DIR"/tmp.tar.gz ]; then
+    curl -s -LJO $LATEST
+    tar -xzvf node*.tar.gz
+    mv node*.tar.gz tmp.tar.gz
 fi
 
-cd "$NODE_DIR"
+cd node*
 
 ./node_exporter
